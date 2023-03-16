e4eb71b3486249e34fd1b19ffc4a8e898a15f6c0
Aed10 <122780818+Aed10@users.noreply.github.com>
2023-03-16 13:12:47 +0100

    Update readme.sh

--- readme.sh
+++ readme.sh
@@ -1,14 +1,12 @@
- #!/bin/bash                                                               2
-  3 # Ask user for project name and description
-  4 echo "Enter project name: "
-  5 read project_name
-  6
-  7 echo "Enter project description: "
-  8 read project_description
-  9
- 10 # Create README.md file with project name and description
- 11 echo "# $project_name" > README.md
- 12 echo "" >> README.md
- 13 echo "$project_description" >> README.md
- 14
- 15 echo "README.md created successfully!"
+#!/bin/bash                                                               2
+# Ask user for project name and description
+echo "Enter project name: "
+read project_name
+echo "Enter project description: "
+read project_description
+
+# Create README.md file with project name and description
+echo "# $project_name" > README.md
+echo "" >> README.md
+echo "$project_description" >> README.md
+echo "README.md created successfully!"
